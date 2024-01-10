1d80f734416ca24082219d8303e746788f0b7ab3
randy <147568010+RunD7@users.noreply.github.com>
2024-01-10 14:00:23 +0700

    tambah .php

---
+++ app/Views/pages/ta1.php
@@ -0,0 +1,84 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<link rel="stylesheet" href="/css/ta1.css">
+<div class="form-main-wrapper">
+    <div>
+        <img>
+        <form>
+            <h1 class="mt-5 mb-5">Formulir Tugas Akhir 1 Mahasiswa Informatika UNJANI</h1>
+            <div class="form-input-wrapp form-mb-3">
+                <label for="firstname" class="form-form-label"> Name </label>
+
+                <div>
+                    <input type="text" name="firstname" id="firstname" placeholder="First name" class="form-form-input" />
+
+                    <input type="text" name="lastname" id="lastname" placeholder="Last name" class="form-form-input" />
+                </div>
+            </div>
+
+            <div class="form-mb-3">
+                <label for="age" class="form-form-label"> Age </label>
+                <input type="text" name="age" id="age" placeholder="ex:25" class="form-form-input" />
+            </div>
+
+            <div class="form-mb-3">
+                <label for="dob" class="form-form-label"> Date of Birth </label>
+                <input type="date" name="dob" id="dob" class="form-form-input" />
+            </div>
+
+            <div class="form-mb-3">
+                <label class="form-form-label">Gender</label>
+
+                <select class="form-form-input" name="occupation" id="occupation">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                </select>
+            </div>
+
+            <div class="form-mb-3">
+                <label for="email" class="form-form-label"> Email </label>
+                <input type="email" name="email" id="email" placeholder="example@email.com" class="form-form-input" />
+            </div>
+
+            <div class="form-mb-3">
+                <label for="address" class="form-form-label"> Address </label>
+
+                <input type="text" name="address" id="address" placeholder="Street address" class="form-form-input form-mb-3" />
+                <input type="text" name="address2" id="address2" placeholder="Street address line 2" class="form-form-input" />
+            </div>
+
+            <div class="form-mb-3 form-input-wrapp">
+                <label for="phone" class="form-form-label"> Phone </label>
+
+                <div>
+                    <input type="text" name="areacode" id="areacode" placeholder="Area code" class="form-form-input form-w-45" />
+
+                    <input type="text" name="phone" id="phone" placeholder="Phone number" class="form-form-input" />
+                </div>
+            </div>
+
+            <div class="form-input-flex">
+                <div>
+                    <label for="post" class="form-form-label"> Post/Zip code </label>
+                    <input type="text" name="post" id="post" placeholder="ex:8976" class="form-form-input" />
+                </div>
+                <div>
+                    <label for="city" class="form-form-label"> City </label>
+                    <input type="text" name="city" id="city" placeholder="ex: New York" class="form-form-input" />
+                </div>
+            </div>
+
+            <div class="form-mb-3">
+                <label for="upload" class="form-form-label">
+                    Upload Signature
+                </label>
+                <input type="file" name="upload" id="upload" class="form-form-input form-form-file" />
+            </div>
+
+            <button class="btn btn-primary mt-4">Submit</button>
+        </form>
+    </div>
+</div>
+<?= $this->endSection(); ?>
