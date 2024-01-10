75ac8fc6b97fea5d6343145501a5094661bfdea7
saarisdi <69184469+saarisdi@users.noreply.github.com>
2024-01-10 13:15:48 +0700

    Menambahkan beberapa file untuk mengatur layout
    File ini yang berfungsi sebagai layouting pada Views pages

---
+++ navbar.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar fixed-top navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <img src="/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <a class="navbar-brand" href="#">
+            Pendaftaran TA IF UNJANI
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Beranda</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Tugas Akhir
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="/pages/ta1">TA1</a></li>
+                        <li><a class="dropdown-item" href="/pages/ta2">TA2</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="/pages/dosen">Dosen</a></li>
+                    </ul>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/pages/jadwal">Jadwal</a>
+                </li>
+            </ul>
+            <a href="/pages/login" class="btn btn-success">Masuk</a>
+        </div>
+    </div>
+</nav>
