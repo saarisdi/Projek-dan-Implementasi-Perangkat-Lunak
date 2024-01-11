1decdd8bcdc516cac7b97d95f8fb79863ddcd836
maulanaanjasmoro <153880660+maulanaanjasmoro@users.noreply.github.com>
2024-01-11 15:48:50 +0700

    Update1 login.php
    merubah login menggunakan libraly

--- app/Views/pages/login.php
+++ app/Views/pages/login.php
@@ -1,26 +1,74 @@
-<?= $this->extend('layout/template'); ?>
+<?= $this->extend($config->viewLayout) ?>
+<?= $this->section('main') ?>
 
-<?= $this->section('content'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-6 offset-sm-3">
 
-<body>
-    <div class="d-flex justify-content-center mt-5 pt-5">
-        <div class="card text-center col-sm-3">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <form class="form-inline">
-                    <label class="sr-only" for="inlineFormInputName2">Name</label>
-                    <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Jane Doe">
-                    <label class="sr-only" for="inlineFormInputGroupUsername2">Username</label>
-                    <div class="input-group mb-2 mr-sm-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">@</div>
-                        </div>
-                        <input type="text" class="form-control" id="inlineFormInputGroupUsername2" placeholder="Username">
-                    </div>
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</body>
-<?= $this->endSection(); ?>
+			<div class="card">
+				<h2 class="card-header"><?=lang('Auth.loginTitle')?></h2>
+				<div class="card-body">
+
+					<?= view('Myth\Auth\Views\_message_block') ?>
+
+					<form action="<?= url_to('login') ?>" method="post">
+						<?= csrf_field() ?>
+
+<?php if ($config->validFields === ['email']): ?>
+						<div class="form-group">
+							<label for="login"><?=lang('Auth.email')?></label>
+							<input type="email" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
+								   name="login" placeholder="<?=lang('Auth.email')?>">
+							<div class="invalid-feedback">
+								<?= session('errors.login') ?>
+							</div>
+						</div>
+<?php else: ?>
+						<div class="form-group">
+							<label for="login"><?=lang('Auth.emailOrUsername')?></label>
+							<input type="text" class="form-control <?php if (session('errors.login')) : ?>is-invalid<?php endif ?>"
+								   name="login" placeholder="<?=lang('Auth.emailOrUsername')?>">
+							<div class="invalid-feedback">
+								<?= session('errors.login') ?>
+							</div>
+						</div>
+<?php endif; ?>
+
+						<div class="form-group">
+							<label for="password"><?=lang('Auth.password')?></label>
+							<input type="password" name="password" class="form-control  <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?=lang('Auth.password')?>">
+							<div class="invalid-feedback">
+								<?= session('errors.password') ?>
+							</div>
+						</div>
+
+<?php if ($config->allowRemembering): ?>
+						<div class="form-check">
+							<label class="form-check-label">
+								<input type="checkbox" name="remember" class="form-check-input" <?php if (old('remember')) : ?> checked <?php endif ?>>
+								<?=lang('Auth.rememberMe')?>
+							</label>
+						</div>
+<?php endif; ?>
+
+						<br>
+
+						<button type="submit" class="btn btn-primary btn-block"><?=lang('Auth.loginAction')?></button>
+					</form>
+
+					<hr>
+
+<?php if ($config->allowRegistration) : ?>
+					<p><a href="<?= url_to('register') ?>"><?=lang('Auth.needAnAccount')?></a></p>
+<?php endif; ?>
+<?php if ($config->activeResetter): ?>
+					<p><a href="<?= url_to('forgot') ?>"><?=lang('Auth.forgotYourPassword')?></a></p>
+<?php endif; ?>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<?= $this->endSection() ?>
